1b273599dd4cb2cd246f580372d925d578691656
Mike-Benn <mbenn49@wgu.edu>
2024-08-03 02:56:41 -0400

    change port variable

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use("/", router);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, "0.0.0.0", () => {
+const port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", () => {
     console.log(`My first Express app - listening on port ${PORT}!`);
   });
