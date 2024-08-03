e87128779c541857f1a396d843e319509addead7
Mike-Benn <mbenn49@wgu.edu>
2024-08-03 00:43:32 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const router = require("./routes/router");
+
+
+app.set("views", path.join(__dirname , "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use("/", router);
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`My first Express app - listening on port ${PORT}!`));
---
+++ routes/router.js
@@ -0,0 +1,32 @@
+const { Router } = require("express");
+
+const router = Router();
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
+    }
+];
+
+router.get("/" , (req, res) => {
+    res.render("index" , { messages: messages });
+});
+
+router.get("/new" , (req , res) => {
+    res.render("form");
+})
+
+router.post("/new" , (req , res) => {
+    messages.push({ text: req.body.messageText, user: req.body.authorName, added: new Date() });
+    res.redirect("/");
+})
+
+
+
+module.exports = router;
