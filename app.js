db5d05db84fd1e1eb00beb246bcf5d6c1250982a
Mike-Benn <mbenn49@wgu.edu>
2024-08-03 02:21:58 -0400

    fix app.listen to adhere to railway requirements

--- app.js
+++ app.js
@@ -10,5 +10,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use("/", router);
 
-const PORT = 3000;
-app.listen(PORT, "0.0.0.0", () => console.log(`My first Express app - listening on port ${PORT}!`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`My first Express app - listening on port ${PORT}!`);
+  });
